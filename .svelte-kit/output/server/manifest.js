38114cdd0c6be21080c9ba8bb922e8cc5df752a9
Yifan Sun <sunyifan112358@gmail.com>
2023-06-14 12:36:56 -0400

    Use svelte

---
+++ .svelte-kit/output/server/manifest.js
@@ -0,0 +1,27 @@
+export const manifest = {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.72f1829c.js","app":"_app/immutable/entry/app.f1208d35.js","imports":["_app/immutable/entry/start.72f1829c.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.fe037310.js","_app/immutable/entry/app.f1208d35.js","_app/immutable/chunks/index.6dba6488.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			() => import('./nodes/0.js'),
+			() => import('./nodes/1.js'),
+			() => import('./nodes/2.js')
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+};
---
+++ .svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,7 @@
+
+
+export const index = 1;
+export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.0e42448f.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.fe037310.js"];
+export const stylesheets = [];
+export const fonts = [];
