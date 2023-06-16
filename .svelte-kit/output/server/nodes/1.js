3213b84ebae790c28583e2a92fce0710c02e5f07
Yifan Sun <sunyifan112358@gmail.com>
2023-06-16 15:01:54 -0400

    Add netlify.toml

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.5d4820d2.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.a00b61d5.js"];
-export const stylesheets = [];
-export const fonts = [];
