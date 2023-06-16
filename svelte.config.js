959c74da1f30c811cc628b96dad68387bc8b7f92
Yifan Sun <sunyifan112358@gmail.com>
2023-06-16 15:32:27 -0400

    it seems working

--- svelte.config.js
+++ svelte.config.js
@@ -11,14 +11,8 @@ const config = {
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
-            // if true, will create a Netlify Edge Function rather
-            // than using standard Node-based functions
             edge: false,
-
-            // if true, will split your app into multiple functions
-            // instead of creating a single one for the entire app.
-            // if `edge` is true, this option cannot be used
-            split: false,
+            split: true,
         }),
         outDir: 'publish',
         alias: {
