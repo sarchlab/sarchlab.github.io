682da8e83ce6d3ba99fd1be429f030798ec91f92
Yifan Sun <sunyifan112358@gmail.com>
2024-08-20 11:36:14 -0400

    None wildcard redirect

--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BIOIggOs.js","_app/immutable/chunks/scheduler.DUa3pFyD.js","_app/immutable/chunks/index.CS9allBQ.js","_app/immutable/chunks/stores.D0Uvv-7u.js","_app/immutable/chunks/entry.CbTx-y0t.js"];
+export const imports = ["_app/immutable/nodes/1.B5BAxoZw.js","_app/immutable/chunks/scheduler.DUa3pFyD.js","_app/immutable/chunks/index.CS9allBQ.js","_app/immutable/chunks/stores.DcQUSdoa.js","_app/immutable/chunks/entry.gHcnEYIA.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .netlify/server/nodes/2.js
+++ .netlify/server/nodes/2.js
@@ -3,6 +3,6 @@
 export const index = 2;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/(cams)/cams24/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.CPV_qr_G.js","_app/immutable/chunks/scheduler.DUa3pFyD.js","_app/immutable/chunks/index.CS9allBQ.js","_app/immutable/chunks/footer.DApOdTvp.js","_app/immutable/chunks/each.D6YF6ztN.js","_app/immutable/chunks/nav.svelte_svelte_type_style_lang.DzaR5pMu.js","_app/immutable/chunks/stores.D0Uvv-7u.js","_app/immutable/chunks/entry.CbTx-y0t.js"];
+export const imports = ["_app/immutable/nodes/2.DWNtwxXl.js","_app/immutable/chunks/scheduler.DUa3pFyD.js","_app/immutable/chunks/index.CS9allBQ.js","_app/immutable/chunks/footer.Djh2zmVE.js","_app/immutable/chunks/each.D6YF6ztN.js","_app/immutable/chunks/nav.svelte_svelte_type_style_lang.DzaR5pMu.js","_app/immutable/chunks/stores.DcQUSdoa.js","_app/immutable/chunks/entry.gHcnEYIA.js"];
 export const stylesheets = ["_app/immutable/assets/footer.Dx2yb4HH.css","_app/immutable/assets/app.BNvxSZ51.css","_app/immutable/assets/nav.CLsN7H2l.css"];
 export const fonts = [];
