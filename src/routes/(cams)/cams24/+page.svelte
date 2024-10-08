112ff3be8d8688bbfcd17a50261126e249804d0f
Yifan Sun <sunyifan112358@gmail.com>
2024-10-08 13:01:38 -0400

    Update cams24 tutorial

--- src/routes/(cams)/cams24/+page.svelte
+++ src/routes/(cams)/cams24/+page.svelte
@@ -147,66 +147,89 @@
 <div class="block">
     <div class="block-content">
         <h2 class="text-theme font-bold mb-6">Workshop Program</h2>
-        <p>
-            All times are in Central Standard Time (UTC-6).
-        </p>
+        <p>All times are in Central Standard Time (UTC-6).</p>
         <table id="schedule">
-            <tr>
-                <td>8:00 - 8:10</td>
-                <td>Opening Remarks</td>
-            </tr>
-            <tr>
-                <td>8:10 - 9:00</td>
-                <td>Keynote</td>
-            </tr>
-            <tr>
-                <td>9:00 - 10:00</td>
-                <td>Paper Talks</td>
-            </tr>
-            <tr class="sub-schedule">
-                <td></td>
-                <td>9:00 - 9:15  |  Demystifying Platform Requirements for Diverse LLM Inference Use Cases</td>
-            </tr>
-            <tr class="sub-schedule">
-                <td></td>
-                <td>9:15 - 9:30  |  BottleneckAI: Harnessing Machine Learning and Knowledge Transfer for Detecting Architectural Bottlenecks</td>
-            </tr>
-            <tr class="sub-schedule">
-                <td></td>
-                <td>9:30 - 9:45  |  How Accurate is Accurate Enough for Simulators? A Review of Simulation Validation</td>
-            </tr>
-            <tr class="sub-schedule">
-                <td></td>
-                <td>9:45 - 10:00  |  Parallelizing a Modern GPU Simulator</td>
-            </tr>
-            <tr>
-                <td>10:00 - 10:30</td>
-                <td>Coffee break</td>
-            </tr>
-            <tr>
-                <td>10:30 - 12:00</td>
-                <td>Simulator Talks</td>
-            </tr>
-            <tr class="sub-schedule">
-                <td></td>
-                <td>10:30 - 10:50  |  gem5 (Jason Lowe-Power)</td>
-            </tr>
-            <tr class="sub-schedule">
-                <td></td>
-                <td>10:50 - 11:10  |  Sniper (Alen Sabu, Trevor E. Carlson)</td>
-            </tr>
-            <tr class="sub-schedule">
-                <td></td>
-                <td>11:10 - 11:30  |  Akita (Yifan Sun)</td>
-            </tr>
-            <tr class="sub-schedule">
-                <td></td>
-                <td>11:30 - 11:50  |  SST (TBD)</td>
-            </tr>
-            <tr>
-                <td>11:50 - 12:00</td>
-                <td>Closing Remarks</td>
-            </tr>
+            <thead>
+                <tr>
+                    <td>Time</td>
+                    <td>Event</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>8:00 - 8:10</td>
+                    <td>Opening Remarks</td>
+                </tr>
+                <tr>
+                    <td>8:10 - 9:00</td>
+                    <td>Keynote</td>
+                </tr>
+                <tr>
+                    <td>9:00 - 10:00</td>
+                    <td>Paper Talks</td>
+                </tr>
+                <tr class="sub-schedule">
+                    <td>9:00 - 9:15</td>
+                    <td>
+                        Demystifying Platform Requirements for Diverse LLM
+                        Inference Use Cases
+                    </td>
+                </tr>
+                <tr class="sub-schedule">
+                    <td>9:15 - 9:30</td>
+                    <td>
+                        BottleneckAI: Harnessing Machine Learning and Knowledge
+                        Transfer for Detecting Architectural Bottlenecks
+                    </td>
+                </tr>
+                <tr class="sub-schedule">
+                    <td>9:30 - 9:45</td>
+                    <td>
+                        How Accurate is Accurate Enough for Simulators? A
+                        Review of Simulation Validation
+                    </td>
+                </tr>
+                <tr class="sub-schedule">
+                    <td>9:45 - 10:00</td>
+                    <td>Parallelizing a Modern GPU Simulator</td>
+                </tr>
+                <tr>
+                    <td>10:00 - 10:30</td>
+                    <td>Coffee break</td>
+                </tr>
+                <tr>
+                    <td>10:30 - 12:00</td>
+                    <td>Simulator Talks</td>
+                </tr>
+                <tr class="sub-schedule">
+                    <td>10:30 - 10:50</td>
+                    <td>gem5 (Jason Lowe-Power)</td>
+                </tr>
+                <tr class="sub-schedule">
+                    <td>10:50 - 11:10</td>
+                    <td>Sniper (Alen Sabu, Trevor E. Carlson)</td>
+                </tr>
+                <tr class="sub-schedule">
+                    <td>11:10 - 11:30</td>
+                    <td>
+                        User-Friendly Tools in Akita (Yifan Sun)
+                        <p class="talk-abstract">
+                            In this talk, we will present the real-time
+                            monitoring tool for Akita---AkitaRTM---and the
+                            default trace visualization tool for
+                            Akita---Daisen.
+                        </p>
+                    </td>
+                </tr>
+                <tr class="sub-schedule">
+                    <td>11:30 - 11:50</td>
+                    <td>SST (TBD)</td>
+                </tr>
+                <tr>
+                    <td>11:50 - 12:00</td>
+                    <td>Closing Remarks</td>
+                </tr>
+            </tbody>
         </table>
     </div>
 </div>
@@ -375,9 +398,9 @@
                         >
                     </td>
                     <td>
-                        <a href="" target="&ldquo;blank&rdquo;"
-                            >Sabila Al Jannat</a
-                        >
+                        <a href="" target="&ldquo;blank&rdquo;">
+                            Sabila Al Jannat
+                        </a>
                     </td>
                 </tr>
                 <tr>
@@ -399,15 +422,18 @@
 <div class="block">
     <div class="block-content">
         <h2 class="text-theme font-bold mb-6">Program Committee</h2>
-        <p>In this workshop, we are experimenting with a PhD and practitioner-led PC. 
-            We believe that PhD students and practitioners are the end users of simulation 
-            and performance modeling tools and hence, should know the tools the best. We will 
-            report our experience during the workshop event.</p>
+        <p>
+            In this workshop, we are experimenting with a PhD and
+            practitioner-led PC. We believe that PhD students and practitioners
+            are the end users of simulation and performance modeling tools and
+            hence, should know the tools the best. We will report our
+            experience during the workshop event.
+        </p>
         <ul class="ml-6 list-disc">
             <li>Yuhui Bao (Northeastern University)</li>
             <li>Ying Li (William & Mary)</li>
             <li>Changxi Liu (National University of Singapore)</li>
-            <li>Patrick Lavin (Sandia National Lab) </li>
+            <li>Patrick Lavin (Sandia National Lab)</li>
             <li>Mohammadreza Rezvani (UC Riverside)</li>
             <li>Mahyar Samani (UC Davis)</li>
             <li>William Won (Georgia Institute of Technology)</li>
@@ -421,27 +447,32 @@
         @apply border-t-2 border-b-2 border-primary;
     }
 
-    #schedule > tr {
+    #schedule thead {
+        @apply bg-primary text-white;
+        @apply font-bold;
+    }
+
+    #schedule tr {
         @apply border-b border-primary;
     }
 
-    #schedule > tr > td:first-child {
+    #schedule tr > td:first-child {
         @apply w-3/12;
         /* @apply text-right; */
         @apply font-bold;
     }
 
-    #schedule > tr > td {
+    #schedule tr > td {
         @apply py-2 px-4;
         @apply align-top;
     }
 
-    #schedule > tr.sub-schedule > td {
-        @apply pl-10;
+    #schedule tr.sub-schedule > td {
+        @apply pl-8;
         @apply font-normal;
     }
 
-    #schedule > tr.sub-schedule > td > span.affliation {
+    #schedule tr.sub-schedule > td > span.affliation {
         @apply text-gray-500;
         @apply font-normal text-xs;
     }
@@ -460,6 +491,11 @@
 
     .abstract {
         margin-bottom: 1em;
+    }
+
+    .talk-abstract {
+        @apply text-sm;
+        @apply text-gray-500;
     }
 
     .collapse-opener {
