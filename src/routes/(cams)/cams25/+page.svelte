14d1e7c4e09fd2dab32daeaf7c9fad9ba0177cbc
EnzeXu <xezpku@gmail.com>
2025-10-06 13:37:07 -0400

    update workshop program of CAMS25

--- src/routes/(cams)/cams25/+page.svelte
+++ src/routes/(cams)/cams25/+page.svelte
@@ -277,7 +277,150 @@
 <div class="block">
     <div class="block-content">
         <h2 class="text-theme font-bold mb-6">Workshop Program</h2>
-        <p>TBD</p>
+        <p>All times are in Korea Standard Time (KST, UTC+9).</p>
+        <table id="schedule">
+            <thead>
+                <tr>
+                    <td>Time</td>
+                    <td>Event</td>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>8:00 AM – 8:10 AM</td>
+                <td style="font-weight:bold;">Opening Remarks</td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align:center; font-weight:bold;">Session 1</td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>8:10 AM – 9:00 AM</td>
+                <td>Keynote: Gwangsun Kim</td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>9:00 AM – 9:20 AM</td>
+                <td>
+                    [Paper] NetTLMSim: A Virtual Prototype Simulator for Large-Scale Accelerator Networks
+                    <p>Junsu Heo, Shinyoung Kim, Hyeseong Shin, Jaesuk Lee (Konkuk University), Sungkyung Park (Pusan National University) and Chester Sungchung Park (Konkuk University)</p> 
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>9:20 AM – 9:40 AM</td>
+                <td>
+                    [Paper] A comprehensive analysis and modeling of spill operations in vector processing units
+                    <p>Hossein Mokhtarnia, Osman Unsal and Adrian Cristal Kestelman (Barcelona Supercomputing Center)</p>
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>9:40 AM – 10:00 AM</td>
+                <td>Discussion</td>
+            </tr>
+            <tr>
+                <td>10:00 AM – 10:30 AM</td>
+                <td style="font-weight:bold;">Coffee Break</td>
+            </tr>
+
+            <tr>
+                <td colspan="2" style="text-align:center; font-weight:bold;">Session 2: AI for Simulation</td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>10:30 AM – 10:50 AM</td>
+                <td>
+                    [Paper] gem5 Co-Pilot: AI Assistant Agent for Architectural Design Space Acceleration
+                    <p>Zuoming Fu (Cornell University), Alexander Manley (University of Kansas) and Mohammad Alian (Cornell University)</p>
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>10:50 AM – 11:10 AM</td>
+                <td>
+                    [Paper] DaisenBot: Human-AI Collaboration in GPU Performance Analysis with Multi-Modal AI Assistant [Remote]
+                    <p>Enze Xu, Jeremy Coonley, Daoxuan Xu and Yifan Sun (College of William & Mary)</p>
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>11:10 AM – 11:30 AM</td>
+                <td>Santosh [Talk title TBD, remote]</td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>11:30 AM – 11:50 AM</td>
+                <td>Discussion</td>
+            </tr>
+
+            <tr>
+                <td>12:00 PM – 1:00 PM</td>
+                <td style="font-weight:bold;">Lunch Break</td>
+            </tr>
+            
+            <tr>
+                <td colspan="2" style="text-align:center; font-weight:bold;">Session 3: Network on chip and system-level simulation</td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>1:00 PM – 1:20 PM</td>
+                <td>
+                    [Paper] A Cycle-accurate, Extensible Traffic Generator for Cross-abstraction NoC Simulation
+                    <p>Chanwoo Song and Hyun-Gyu Kim (Openedges Technology)</p>
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>1:20 PM – 1:40 PM</td>
+                <td>
+                    [Paper] Latency-Aware QoS Optimization of XY-YX Routing in NoCs via Analytical Latency Estimation
+                    <p>Jongwon Oh, Seongmo An, Jinyoung Shin and Seung Eun Lee (Seoul National University of Science and Technology)</p>
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>1:40 PM – 2:00 PM</td>
+                <td>
+                    [Paper] Enabling Realistic Virtualized Cloud Workload Evaluation in RISC-V
+                    <p>Nikos Karystinos, George-Marios Fragkoulis and Dimitris Gizopoulos (University of Athens)</p>
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>2:00 PM – 2:20 PM</td>
+                <td>[TBD]</td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>2:20 PM – 2:40 PM</td>
+                <td>Discussion</td>
+            </tr>
+
+            <tr>
+                <td>3:00 PM – 3:30 PM</td>
+                <td style="font-weight:bold;">Coffee Break</td>
+            </tr>
+
+            <tr>
+                <td colspan="2" style="text-align:center; font-weight:bold;">Session 4: Simulation Design Methods</td>
+            </tr>
+            
+            <tr class="sub-schedule">
+                <td>3:30 PM – 3:50 PM</td>
+                <td>
+                    [Paper] Phalanx: A Processor Simulator Based on the Entity Component System Architecture
+                    <p>Toshiki Maekawa (Nagoya Institute of Technology), Akihiko Odaki (The University of Tokyo), Toru Koizumi, Tomoaki Tsumura (Nagoya Institute of Technology) and Ryota Shioya (The University of Tokyo)</p>
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>3:50 PM – 4:10 PM</td>
+                <td>
+                    [Tool Release Talk] Mess Simulator: New Capabilities in the Latest Release
+                    <p>Pouya Esmaili-Dokht, Ashkan Asgharzadeh, Petar Radojkovic and Eduard Ayguadé (Barcelona Supercomputing Center)</p>
+                </td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>4:10 PM – 4:30 PM</td>
+                <td>Rishov Sarkar [Talk title TBD]</td>
+            </tr>
+            <tr class="sub-schedule">
+                <td>4:30 PM – 4:50 PM</td>
+                <td>Discussion</td>
+            </tr>
+            <tr>
+                <td>4:50 PM – 5:00 PM</td>
+                <td style="font-weight:bold;">Closing Remarks</td>
+            </tr>
+        </tbody>
+        </table>
     </div>
 </div>
 
